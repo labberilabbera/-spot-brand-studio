8e6b85791eed6c488c6b167062925fd7f14e932c
labberilabbera <labberilabbera@gmail.com>
2026-06-17 08:56:31 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import { readFileSync } from 'fs'
 import { join } from 'path'
-import { NextResponse } from 'next/server'
 
 export const dynamic = 'force-dynamic'
 
 export default function Home() {
   const html = readFileSync(join(process.cwd(), 'poc.html'), 'utf-8')
-  return new NextResponse(html, {
-    headers: { 'Content-Type': 'text/html; charset=utf-8' },
-  }) as any
+  return <div dangerouslySetInnerHTML={{ __html: html }} />
 }
